eade3d89d56e0d75ac90bf294378c6a8120c90a5
Fadhil Muhammad <fadhil.setiawan@tech.jago.com>
2025-11-14 02:27:35 +0700

    ex2: plus some formatting with clang

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -1,11 +1,18 @@
+/*
+ * Copyright (c) 2025 Fadhil Muhammad. All Rights Reserved.
+ */
+
 #include <stdio.h>
 
 /* This is a comment */
 int main(int argc, char *argv[]) {
-  int distance = 100;
+    int distance = 100;
 
-  // this is also a comment
-  printf("You are %d kilometers away.\n", distance);
+    // this is also a comment
+    printf("You are %d kilometers away.\n", distance);
+    printf("You are %d kilometers away.\n", distance);
+    printf("You are %d kilometers away.\n", distance);
+    printf("You are %d kilometers away.\n", distance);
 
-  return 0;
+    return 0;
 }
---
+++ ex2/ex2.c
@@ -0,0 +1,11 @@
+// Copyright (c) 2025 Fadhil Muhammad. All Rights Reserved.
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    char name[] = "padul";
+
+    printf("Hello there, %s!\n", name);
+
+    return EXIT_SUCCESS;
+}
