391faafc14107e03a427839f35aa7149fcbd99f2
Fadhil Muhammad <fadhil.setiawan@tech.jago.com>
2025-11-15 02:53:09 +0700

    continue

---
+++ ex3/ex3.c
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2025 Fadhil Muhammad. All Rights Reserved.
+ */
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int age = 10;
+    int height = 208;
+    double clocks = 3.4;
+
+    printf("I am %d years old!\n", age);
+    printf("I am %d centimeters tall!\n", height);
+    printf("The clocks is now %.5g\n", clocks);
+
+    return 0;
+}
