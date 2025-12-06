f35316fc7d1f7968aeca6f48f0d66672917327db
Fadhil Muhammad <fadhil.setiawan@tech.jago.com>
2025-12-07 01:37:34 +0700

    reset

---
+++ reset.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# need to create a delete mechanism that deletes every compiled app in every
+# exercise folder like e.g `ex11/ex11` and keeping only the `Makefile` and the
+# `*.c` file.
+
+# Source - https://stackoverflow.com/a/8512497
+# Posted by Bennet Yee, modified by community. See post 'Timeline' for change history
+# Retrieved 2025-12-07, License - CC BY-SA 4.0
+
+read -p "are you sure you want to delete every compilation? (yes/no) " -r
+
+if [[ $REPLY =~ ^[Yy][Ee][Ss]$ ]]; then
+  find ex*/ex* -maxdepth 2 -type f ! -name "*.c" -exec echo "rm -rf {}" \; -exec rm -rf "{}" \;
+elif [[ $REPLY =~ ^[Nn][Oo]$ ]]; then
+  printf "deletion aborted \n"
+  exit 1
+else
+  printf "invalid input. Please enter 'yes' or 'no'.\n"
+fi
+
+exit 0
+
+
