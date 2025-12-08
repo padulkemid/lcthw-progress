1d2bd3b04049a398c5aa9e5656395d6b6f75a0cf
Fadhil Muhammad <fadhil.setiawan@tech.jago.com>
2025-12-09 00:58:29 +0700

    new.sh comments

--- new.sh
+++ new.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # cp the make file inside `ex10` that already has all the flags created
-
+# and in this script it will actually make the file easier so I don't manipulate
+# manually for Makefile and the actual .c file
+# I think while learning c, I could make this in c why I make it in shell
+# script, idk...
 if [[ -n $1 ]]; then
   mkdir $1
   cp ./ex10/Makefile ./$1
